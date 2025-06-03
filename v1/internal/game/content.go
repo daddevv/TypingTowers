9f5fce6de23102e381d593f5eb7a33ebb63f6322
Michael <87670807+daddevv@users.noreply.github.com>
2025-06-03 15:06:24 -0400

    Add basic tower, mobs, and projectile system

--- v1/internal/game/content.go
+++ v1/internal/game/content.go
@@ -1,6 +1,7 @@
 package game
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -12,6 +13,10 @@ var (
 	ImgBackgroundTile       = loadImage("assets/basic_tile_32.png")
 	ImgHighlightTile        = loadImage("assets/basic_tile_highlight_32.png")
 	ImgHouseTile            = loadImage("assets/blue_house_32.png")
+	ImgTower                = generateTowerImage()
+	ImgMobA                 = generateMobImage(color.RGBA{255, 0, 0, 255})
+	ImgMobB                 = generateMobImage(color.RGBA{255, 128, 0, 255})
+	ImgProjectile           = generateProjectileImage()
 )
 
 // loadImage is the utility function to load an image from a file path.
@@ -40,6 +45,32 @@ func generateBackground() *ebiten.Image {
 			bg.DrawImage(tile, op)
 		}
 	}
-	
 	return bg
+}
+
+func generateTowerImage() *ebiten.Image {
+	img := ebiten.NewImage(32, 32)
+	img.Fill(color.RGBA{0, 0, 200, 255})
+	return img
+}
+
+func generateMobImage(c color.Color) *ebiten.Image {
+	img := ebiten.NewImage(32, 32)
+	img.Fill(c)
+	return img
+}
+
+func generateProjectileImage() *ebiten.Image {
+	img := ebiten.NewImage(8, 8)
+	clr := color.RGBA{255, 255, 0, 255}
+	for x := 0; x < 8; x++ {
+		for y := 0; y < 8; y++ {
+			dx := x - 4
+			dy := y - 4
+			if dx*dx+dy*dy <= 16 {
+				img.Set(x, y, clr)
+			}
+		}
+	}
+	return img
 }
